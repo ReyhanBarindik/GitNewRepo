b4f6df519d86e300b954fff821f3a031eff1ea0d
ReyhanBarindik <reyhanebarindik@icloud.com>
2020-07-24 08:03:19 -0400

    New changes added

--- src/com/syntax/class1/ClassOne.java
+++ src/com/syntax/class1/ClassOne.java
@@ -3,7 +3,8 @@ package com.syntax.class1;
 public class ClassOne {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 
 	}
 
